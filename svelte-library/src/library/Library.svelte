0ede814bfe7c460afcb4afac48a8ab86f0c71562
Adrian Fearon <adrian.fearon@livetiles.nyc>
2020-04-06 16:20:25 +0100

    css syuff

--- svelte-library/src/library/Library.svelte
+++ svelte-library/src/library/Library.svelte
@@ -1,3 +1,34 @@
-<script></script>
-<style></style>
-<div>Hello Library</div>
+<script>
+import Button from '../common/button.svelte';
+</script>
+
+<style>
+      header {
+    margin: var(--spacingMedium) 0 var(--spacingLarge) 0;
+    text-transform: uppercase;
+  }
+  .preamble {
+    display: block;
+  }
+  h1 {
+    font-size: var(--typeSizeXXLarge);
+    font-weight: var(--typeWeightBold);
+    line-height: var(--typeLineHeightTight);
+  }
+
+  .greeting {
+    font-size: var(--typeSizeSmall);
+  }
+
+</style>
+
+<header>
+<span class="preamble">Welcome to the</span>
+<h1>Library</h1>
+</header>
+
+<p class="greeting">
+This is a library for the people. Read the books here. Go home, and share them with you family.
+</p>
+
+<Button>+ Add Book</Button>
